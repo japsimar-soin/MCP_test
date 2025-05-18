d5c6423c66022b7bf5d49e262bd1effa6f420b36
japsimar-soin <japsimarsoin2003@gmail.com>
2025-05-19 03:09:49 +0530

    Initialised the project and created basic main.py file for testing simple hello

---
+++ main.py
@@ -0,0 +1,13 @@
+import asyncio
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+print(os.getenv("GEMINI_API_KEY"))
+
+async def main():
+    print("Hello from mcp-crash-course!")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
